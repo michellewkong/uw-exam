f6ceb5dd877df9816bbfe0fbe4163017a3199173
ceamoroz <ceamoroz@gmail.com>
2014-03-28 22:58:07 -0400

    added course list for autocomplete

--- web/search.js
+++ web/search.js
@@ -1,4 +1,5 @@
 var courseData = {};
+var courseList = {};
 
 load = function () {
       $.getJSON("https://api.uwaterloo.ca/v2/terms/1139/examschedule.json?key=211902c1630ca71d306f1b40daa5de90",
@@ -11,8 +12,9 @@ load = function () {
             	courseData[i].start_time=d.data[i].sections[0].start_time;
             	courseData[i].end_time=d.data[i].sections[0].end_time;
             	courseData[i].location=d.data[i].sections[0].location;
+				courseList[i] = d.data[i].course;
             }
-			console.log(courseData);
+			console.log(courseList);
           } else {
             console.log("Failed to read course data." + JSON.stringify(d.meta));
           }
